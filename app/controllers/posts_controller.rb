5c750fd43f172eaaf2b295fd0dcbe843f3ed29d6
Emirhan Kaplan <emskaplann@gmail.com>
2019-10-11 22:34:21 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,8 +18,9 @@ class PostsController < ApplicationController
   end
 
   def update
+    byebug
     @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+    @post.update(title: params[:post][:title], description: params[:post][:description])
     redirect_to post_path(@post)
   end
 
